5a28666ba58dd18d88f30437296efa8752ba6e38
Amelia Wibisono <ameliawibi@gmail.com>
2022-03-03 00:16:27 +0800

    get note

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+import express from "express";
+import pg from "pg";
+import methodOverride from "method-override";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(cookieParser());
+
+app.get("/note", (req, res) => {
+  res.render("postNote");
+});
+
+app.listen(3006);
