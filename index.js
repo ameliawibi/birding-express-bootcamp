1f1313c2a9e3c29a7cec908f335754c9e743b4eb
Amelia Wibisono <ameliawibi@gmail.com>
2022-03-04 14:00:59 +0800

    separate behaviour

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post("/note", (req, res) => {
   let datetime = notes.date.concat(" ", notes.time);
   //console.log(datetime);
   let sqlQuery =
-    "INSERT INTO notes (date_time,photo_url,flock_size,vocalization,habitat,behaviour) VALUES ($1, $2, $3, $4, $5, $6)";
+    "INSERT INTO notes (date_time,photo_url,flock_size,vocalization,habitat) VALUES ($1, $2, $3, $4, $5)";
 
   let inputData = [
     datetime,
@@ -51,10 +51,30 @@ app.post("/note", (req, res) => {
     Number(notes.flock_size),
     notes.vocalization,
     notes.habitat,
-    notes.behaviour,
   ];
   pool.query(sqlQuery, inputData, whenQueryDone);
   res.send("post success");
+});
+
+app.get("/note/:id", (req, res) => {
+  const { notesid } = req.params;
+  let sqlQuery = `SELECT * FROM notes INNER JOIN behaviour ON notes.id = behaviour.notes_id`;
+
+  pool.query(sqlQuery, (error, result) => {
+    if (error) {
+      console.log("Error executing query", error.stack);
+      return;
+    }
+    if (result.rows) {
+      // this is the output
+      //console.table(result.rows);
+
+      let ejsData = result.rows;
+      console.table(ejsData);
+      res.send("get success");
+      //res.render("viewNote", ejsData);
+    }
+  });
 });
 
 app.listen(3006);
--- init_tables.sql
+++ init_tables.sql
@@ -1,4 +1,6 @@
+DROP TABLE IF EXISTS behaviour;
 DROP TABLE IF EXISTS notes;
+
 
 CREATE TABLE notes (
 id SERIAL PRIMARY KEY,
@@ -6,11 +8,18 @@ date_time TEXT,
 photo_url TEXT,
 flock_size INTEGER,
 vocalization TEXT,
-habitat TEXT,
-behaviour VARCHAR
+habitat TEXT
 );
 
-INSERT INTO notes (date_time,photo_url,flock_size,vocalization,habitat,behaviour) VALUES ('03/04/2022 12:00 PM','https://picsum.photos/id/1024/300/200','30','vocalization value','habitat value', ARRAY[ 'Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5' ]);
-INSERT INTO notes (date_time,photo_url,flock_size,vocalization,habitat,behaviour) VALUES ('03/04/2022 12:00 PM','https://picsum.photos/id/224/300/200','30','vocalization value','habitat value', ARRAY[ 'Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5' ]);
-INSERT INTO notes (date_time,photo_url,flock_size,vocalization,habitat,behaviour) VALUES ('03/04/2022 12:00 PM','https://picsum.photos/id/269/300/200','30','vocalization value','habitat value', ARRAY[ 'Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5' ]);
-INSERT INTO notes (date_time,photo_url,flock_size,vocalization,habitat,behaviour) VALUES ('03/04/2022 12:00 PM','https://picsum.photos/id/275/300/200','30','vocalization value','habitat value', ARRAY[ 'Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5' ]);
+CREATE TABLE behaviour (
+  id SERIAL PRIMARY KEY,
+  actions TEXT,
+  notes_id INTEGER REFERENCES notes(id)
+);
+
+INSERT INTO notes (date_time,photo_url,flock_size,vocalization,habitat) VALUES ('03/04/2022 12:00 PM','https://picsum.photos/id/1024/300/200','30','vocalization value','habitat value');
+INSERT INTO notes (date_time,photo_url,flock_size,vocalization,habitat) VALUES ('03/04/2022 12:00 PM','https://picsum.photos/id/224/300/200','30','vocalization value','habitat value');
+INSERT INTO notes (date_time,photo_url,flock_size,vocalization,habitat) VALUES ('03/04/2022 12:00 PM','https://picsum.photos/id/269/300/200','30','vocalization value','habitat value');
+INSERT INTO notes (date_time,photo_url,flock_size,vocalization,habitat) VALUES ('03/04/2022 12:00 PM','https://picsum.photos/id/275/300/200','30','vocalization value','habitat value');
+
+INSERT INTO behaviour (actions,notes_id) VALUES ('drinking','1'),('drinking','2'),('drinking','3'),('drinking','4'),('eating','1'),('eating','3'),('eating','4'),('bathing','2'),('drinking','3'),('drinking','4');
